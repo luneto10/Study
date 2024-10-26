73a74b386260f39b0c0ad70410dfa4bc748962ec
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2024-10-26 16:48:28 -0500

    Add frotEnd Files

---
+++ FrontEnd/React/aluroni-router-aula1.2/src/routes.js
@@ -0,0 +1,23 @@
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default function AppRoutes() {
+  return (
+    <main>
+      <Router >
+        <Menu />
+        
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}> 
+            <Route index element={<Inicio />}/>
+            <Route path="cardapio" element={<Cardapio />}/>
+          </Route>
+
+        </Routes>
+      </Router>
+    </main>
+  );
+}
